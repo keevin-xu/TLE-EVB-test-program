da71ba8c66c675be77364b74b4a83cbaa57171a1
Kevin <kevin.z.xu6@gmail.com>
2026-06-26 18:16:26 -0500

    uart test can use uart2

--- src/uart_hello_world.c
+++ src/uart_hello_world.c
@@ -1,37 +1,56 @@
 #include "tle_device.h"
 #include "uart_hello_world.h"
 
+#define UART_HELLO_USE_UART1 1
+#define UART_HELLO_USE_UART2 0
+
 volatile unsigned long g_uart_hello_loop_count = 0u;
 volatile unsigned long g_uart_hello_chars_sent = 0u;
 volatile unsigned long g_uart_hello_tx_timeouts = 0u;
 
-static void uart1_wait_for_tx_done(void)
+static void uart_wait_for_tx_done(void)
 {
     volatile uint32 timeout;
 
     for (timeout = 0u; timeout < 100000u; timeout++)
     {
+#if UART_HELLO_USE_UART1
         if (UART1_isByteTransmitted() != false)
         {
             return;
         }
+#endif
+
+#if UART_HELLO_USE_UART2
+        if (UART2_isByteTransmitted() != false)
+        {
+            return;
+        }
+#endif
     }
 
     g_uart_hello_tx_timeouts++;
 }
 
-static void uart1_write_char(char c)
+static void uart_write_char(char c)
 {
+#if UART_HELLO_USE_UART1
     UART1_Send_Byte((uint8)c);
+#endif
+
+#if UART_HELLO_USE_UART2
+    UART2_Send_Byte((uint8)c);
+#endif
+
     g_uart_hello_chars_sent++;
-    uart1_wait_for_tx_done();
+    uart_wait_for_tx_done();
 }
 
-static void uart1_write_string(const char *text)
+static void uart_write_string(const char *text)
 {
     while (*text != '\0')
     {
-        uart1_write_char(*text);
+        uart_write_char(*text);
         text++;
     }
 }
@@ -50,7 +69,13 @@ void run_uart_hello_world_demo(void)
     while (1)
     {
         g_uart_hello_loop_count++;
-        uart1_write_string("hello from TLE9855 UART1\r\n");
+#if UART_HELLO_USE_UART1
+        uart_write_string("hello from TLE9855 UART1\r\n");
+#endif
+
+#if UART_HELLO_USE_UART2
+        uart_write_string("hello from TLE9855 UART2\r\n");
+#endif
         delay_loop();
     }
 }
