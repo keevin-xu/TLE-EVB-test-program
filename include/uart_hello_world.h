469f3b335fedef431cc29aef700dc50e64b3697a
Kevin <kevin.z.xu6@gmail.com>
2026-06-26 18:00:53 -0500

    uart_hello_world debug counters

--- include/uart_hello_world.h
+++ include/uart_hello_world.h
@@ -1,6 +1,10 @@
 #ifndef UART_HELLO_WORLD_H
 #define UART_HELLO_WORLD_H
 
+extern volatile unsigned long g_uart_hello_loop_count;
+extern volatile unsigned long g_uart_hello_chars_sent;
+extern volatile unsigned long g_uart_hello_tx_timeouts;
+
 void run_uart_hello_world_demo(void);
 
 #endif
--- src/uart_hello_world.c
+++ src/uart_hello_world.c
@@ -1,16 +1,29 @@
 #include "tle_device.h"
 #include "uart_hello_world.h"
 
+volatile unsigned long g_uart_hello_loop_count = 0u;
+volatile unsigned long g_uart_hello_chars_sent = 0u;
+volatile unsigned long g_uart_hello_tx_timeouts = 0u;
+
 static void uart1_wait_for_tx_done(void)
 {
-    while (UART1_isByteTransmitted() == false)
+    volatile uint32 timeout;
+
+    for (timeout = 0u; timeout < 100000u; timeout++)
     {
+        if (UART1_isByteTransmitted() != false)
+        {
+            return;
+        }
     }
+
+    g_uart_hello_tx_timeouts++;
 }
 
 static void uart1_write_char(char c)
 {
     UART1_Send_Byte((uint8)c);
+    g_uart_hello_chars_sent++;
     uart1_wait_for_tx_done();
 }
 
@@ -36,6 +49,7 @@ void run_uart_hello_world_demo(void)
 {
     while (1)
     {
+        g_uart_hello_loop_count++;
         uart1_write_string("hello from TLE9855 UART1\r\n");
         delay_loop();
     }
